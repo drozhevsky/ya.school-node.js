52c7c814ccc1beed6df60806b2f7d5f373760cc9
piu <fakeacc888@gmail.com>
2017-08-11 00:29:08 +0300

    almost ready

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 
 
-function Form(formName, inputNames) {
+function Form(formName, inputNames, messageDivId) {
 
-    let patterns = {
-        'fio': /\w+ \w+ \w+/,
-        'email': /[a-zA-Z0-9.+@]+@(ya\.ru|yandex\.ru|yandex\.ua|yandex\.by|yandex\.kz|yandex\.com)/,
+    let Patterns = {
+        'fio': /[А-Я][а-я]+ [А-Я][а-я]+ [А-Я][а-я]+/,
+        'email': /[a-zA-Z0-9.+@]+@(ya\.ru|(yandex\.(ru|ua|by|kz|com)))/,
         'phone': /\+7\([0-9]+\)[0-9]{3}-[0-9]{2}-[0-9]{2}/,
     };
 
+    let ErrorMessages = {
+        'fio': 'Формат ФИО: Ровно три слова.',
+        'email': 'Формат E-Mail: E-Mail-адрес, но только в доменах ya.ru, yandex.ru, yandex.ua, yandex.by, yandex.kz, yandex.com.',
+        'phone': 'Формат телефона: Номер телефона, который начинается на +7, и имеет формат +7(999)999-99-99. Кроме того, сумма всех цифр телефона не должна превышать 30.',
+    }
+
     this.validate = () => {
         let errors = this.inputNames.filter((name) => {
-            return this.elements[name].value.match(patterns[name]) == null
+            return this.elements[name].value.match(Patterns[name]) == null
         }, []);
+        const zero = 48  // ord('0')
+        if (!(errors.includes('phone'))) {
+            let phone = this.elements['phone'].value
+            let sum = phone.split('').reduce((acc, letter, i) => {
+                let num = phone.charCodeAt(i) - zero
+                if (num >= 0 && num <= 9)
+                    return acc+num
+                return acc
+            }, 0)
+            console.log(sum)
+            if (sum > 30)
+                errors.push('email')
+        }
         return {isValid: (errors.length == 0), errorFields: errors}
     };
 
@@ -31,12 +50,23 @@ function Form(formName, inputNames) {
 
     this.submit = (e) => {
         e.preventDefault()
+        inputNames.map((name) => {
+            this.elements[name].className = ''
+        })
         let validation = this.validate()
-        if (validation.isValid)
-            console.log('Its okay, can send data')
-        else
-            console.log('Somethings wrong...')
+        if (validation.isValid) {
+            this.showMessage('Its okay, can send data')
+        }
+        else {
+            validation.errorFields.map((name) => {
+                this.elements[name].className = 'error'
+            })
+        }
     };
+
+    this.showMessage = (message) => {
+        this.elements['message'].innerText = message
+    }
 
     this.init = (formName, inputNames) => {
         this.elements = {}
@@ -46,12 +76,9 @@ function Form(formName, inputNames) {
             acc[name] = this.form.elements[name]
             return acc
         }, {})
+        this.elements['message'] = document.getElementById(messageDivId)
         this.form.addEventListener('submit', this.submit)
     };
 
     this.init(formName, inputNames)
-}
-
-function onload(){
-    var MyForm = new Form('myForm', ['fio', 'email', 'phone'])
 }
